a4c2310c0bf08966f8c36d72102e3a5c502f03fd
nathan2626 <61419599+nathan2626@users.noreply.github.com>
2022-06-24 01:29:15 +0200

    ✍🏻 Fix pages content

--- src/screens/Main/Cheeses.screen.js
+++ src/screens/Main/Cheeses.screen.js
@@ -30,77 +30,7 @@ const Cheeses = ({navigation, route}) => {
 
   return (
     <View style={styles.container}>
-      <ScrollView>
-        
-        <Card style={{shadowColor: "#000", shadowOffset: {width: 0, height: 4,}, shadowOpacity: 0.30, shadowRadius: 4.65, elevation: 8, paddingTop:0, marginTop:10, borderRadius:13}}>
-            <View style={{display: 'flex', width: width, backgroundColor: "#fff"}}>
-              <Image source={plant.img} style={{resizeMode: 'contain', flex: 1}} />
-            </View>
-          
-            <View style={{maxWidth: width, flex: 1, flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', width: width * 1, marginTop: 10}}>
-              <Text style={{fontSize: 22, fontWeight: '500', marginLeft:15}}>{plant.name}</Text>
-              <Text style={{fontSize: 20, marginRight:35}}> {plant.price}€</Text>
-            </View>
-
-            <View style={{maxWidth: width, flex: 1, width: width * 1, marginTop: 2, marginBottom: 15}}>
-              <Text style={{color:'orange', fontStyle: 'italic', fontSize: 17, marginLeft: 15, textAlign: 'justify'}}>Comté 12 mois d'affinage</Text>
-            </View>
-
-            <View style={{flex: 1, flexDirection: 'row', alignItems: 'center', justifyContent: 'center'}}>
-                <Text style={{fontSize: 26, marginLeft: 15, marginRight: 5}}>4,9</Text>
-
-              <View style={{flex: 1}}>
-                <Text style={{fontSize: 14, textAlign: 'left'}}>⭐️⭐️⭐️⭐️⭐️</Text>
-                <Text style={{fontSize: 12, textAlign: 'left'}}> 479 Avis</Text>
-              </View>
-            </View>
-            <View style={{maxWidth: width, flex: 1, width: width * 1, marginTop: 15, marginBottom: 10}}>
-              <Text style={{color:'#989898', fontSize: 15, marginRight:35, marginLeft: 15, textAlign: 'justify'}}>{plant.about}</Text>
-            </View>
-
-            <CardAction style={{flex: 1, justifyContent: 'space-between'}} separator={true} inColumn={false}>
-              <CardButton onPress={() => {}} title="➕" />
-              <CardButton style={{}} onPress={() => {}} title="PARTAGER" />
-              <CardButton style={{}} onPress={() => {}} title="❤️" />
-            </CardAction>
-        </Card>
-
-        <Card style={{shadowColor: "#000", shadowOffset: {width: 0, height: 4,}, shadowOpacity: 0.30, shadowRadius: 4.65, elevation: 8, paddingTop:0, marginTop:10, borderRadius:13}}>
-            <View style={{display: 'flex', width: width, backgroundColor: "#fff"}}>
-              <Image source={plant.img} style={{resizeMode: 'contain', flex: 1}} />
-            </View>
-          
-            <View style={{maxWidth: width, flex: 1, flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', width: width * 1, marginTop: 10}}>
-              <Text style={{fontSize: 22, fontWeight: '500', marginLeft:15}}>{plant.name}</Text>
-              <Text style={{fontSize: 20, marginRight:35}}> {plant.price}€</Text>
-            </View>
-
-            <View style={{maxWidth: width, flex: 1, width: width * 1, marginTop: 2, marginBottom: 15}}>
-              <Text style={{color:'orange', fontStyle: 'italic', fontSize: 17, marginLeft: 15, textAlign: 'justify'}}>Comté 12 mois d'affinage</Text>
-            </View>
-
-            <View style={{flex: 1, flexDirection: 'row', alignItems: 'center', justifyContent: 'center'}}>
-                <Text style={{fontSize: 26, marginLeft: 15, marginRight: 5}}>4,9</Text>
-
-              <View style={{flex: 1}}>
-                <Text style={{fontSize: 14, textAlign: 'left'}}>⭐️⭐️⭐️⭐️⭐️</Text>
-                <Text style={{fontSize: 12, textAlign: 'left'}}> 479 Avis</Text>
-              </View>
-            </View>
-            <View style={{maxWidth: width, flex: 1, width: width * 1, marginTop: 15, marginBottom: 10}}>
-              <Text style={{color:'#989898', fontSize: 15, marginRight:35, marginLeft: 15, textAlign: 'justify'}}>{plant.about}</Text>
-            </View>
-
-            <CardAction style={{flex: 1, justifyContent: 'space-between'}} separator={true} inColumn={false}>
-              <CardButton onPress={() => {}} title="➕" />
-              <CardButton style={{}} onPress={() => {}} title="PARTAGER" />
-              <CardButton style={{}} onPress={() => {}} title="❤️" />
-            </CardAction>
-        </Card>
-
-        
-        
-      </ScrollView>
+      <Text style={{ marginTop: 50, textAlign: 'center'}}>Cheeses page</Text>
     </View>
   );
 };
--- src/screens/Main/Search.screen.js
+++ src/screens/Main/Search.screen.js
@@ -22,61 +22,10 @@ const GRADIENT_LOCATIONS = [0.1, 0.9, 0.9, 0.5];
 
 const Search = ({navigation}) => {
   return (
-    <ScrollView style={{flex: 1, backgroundColor:'#ffffff'}} showsVerticalScrollIndicator={false}>
-      <ImageBackground source={require('../../assets/bg.png')} style={{height: height / 2.5}}>
-        <View style={styles.brandView}>
-            <Image source={require('../../assets/CheeseBot-logo.png')} style={{width,resizeMode: 'contain'}} />
-          <Text style={styles.brandViewText}> Fromton</Text>
-        </View>
-      </ImageBackground>
-
-      <View style={styles.bottomView}>
-        <View style={{padding: 40}}>
-
-          <Text style={{color: '#dd4d11', fontSize: 34}}>Bienvenue</Text>
-          <Text>Pas encore de compte ? 
-            <Text style={{color: '#dd4d11', fontStyle: 'italic'}}   onPress={() => navigation.replace('Register')}>
-              {' '} S'enregister
-             </Text>
-          </Text>
-
-          <View style={{marginTop: 50}}>
-            <View style={{paddingBottom: 5, borderBottomWidth: 1, borderColor: '#fde000'}}>
-              <Text style={{color: 'grey', paddingBottom: 3, fontSize: 16}}>Email</Text>
-              <TextInput style={{fontSize: 14}} value="nathanjourno@yahoo.fr" keyboardType='email-address' />
-            </View>
-
-            <View style={{paddingBottom: 5, marginTop: 28, borderBottomWidth: 1, borderColor: '#fde000'}}>
-              <Text style={{color: 'grey', paddingBottom: 3, fontSize: 16}}>Mot de passe</Text>
-              <TextInput style={{fontSize: 14}} value="*********" />
-            </View>
-
-            <View style={{height:100, justifyContent: 'center', alignItems: 'center', marginTop: 32}}>
-              <TouchableOpacity style={styles.loginBtn}>
-                <Text style={{ color: 'black',  fontWeight: 'bold', fontSize: 20,}}>Se connecter</Text>
-              </TouchableOpacity>
-            </View>
-
-            <View style={{flex: 1}}>
-              <Text style={{textAlign: 'center', fontWeight: '700', marginBottom: 26, marginTop: 20}}> ou connectez-vous avec</Text>
-            </View>
-            <View style={{flex: 1, flexDirection: 'row', alignItems: 'center', justifyContent: 'center'}}>
-              <TouchableOpacity
-                style={styles.button2}>
-                <Image source={require('../../assets/facebook.png')} style={styles.icon} />
-              </TouchableOpacity>
-
-              <TouchableOpacity
-                style={styles.buttonGoogle}>
-                <Image source={require('../../assets/google.png')} style={styles.icon} />
-              </TouchableOpacity>
-
-            </View>
-
-          </View>
-
-        </View>
-      </View>
+    <ScrollView style={{flex: 1, backgroundColor:'#ffffff',}} showsVerticalScrollIndicator={false}>
+    <View style={styles.container}>
+      <Text style={{ marginTop: 50, textAlign: 'center'}}>Search page</Text>
+    </View>
 
     </ScrollView>
   );
