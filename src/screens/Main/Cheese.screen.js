150da425f1945d5ba65d2b2fc0b76478bbadd988
nathan2626 <61419599+nathan2626@users.noreply.github.com>
2022-06-24 00:09:48 +0200

    🧀✅ Finalization of the Cheese page

--- src/screens/Main/Cheese.screen.js
+++ src/screens/Main/Cheese.screen.js
@@ -1,187 +1,304 @@
 import React from 'react';
-import {View, SafeAreaView, Image, Text, StyleSheet, Dimensions} from 'react-native';
+import {
+  View,
+  SafeAreaView,
+  Image,
+  Text,
+  StyleSheet,
+  Dimensions,
+} from 'react-native';
 import COLORS from '../../consts/colors';
-import { useNavigation } from '@react-navigation/core';
+import {useNavigation} from '@react-navigation/core';
 import Icon from 'react-native-vector-icons/Ionicons';
-import { ScrollView } from 'react-native-gesture-handler';
+import {ScrollView} from 'react-native-gesture-handler';
 import RecipeResume from './components/recipeResume';
+
+import LinearGradient from 'react-native-linear-gradient';
+
+const START_DEFAULT = {x: 0, y: 1};
+const END_DEFAULT = {x: 0.5, y: 0.5};
+const GRADIENT_COLORS = ['#fde000', '#dd4d11'];
+const GRADIENT_LOCATIONS = [0.9, 0.1, 0.5, 0.5];
+
 const {width, height} = Dimensions.get('window');
 
 const Cheese = () => {
-
   const navigation = useNavigation();
 
   return (
-    <SafeAreaView
-      style={{
-        flex: 1,
-        backgroundColor: COLORS.white,
-      }}>
-      <View style={style.header}>
-        <Icon name='arrow-back' style={{fontSize:32}} onPress={() => navigation.navigate('Home', { screen: 'Cheese' })}/>
-        <Icon name='cart' style={{fontSize:32}}/>
-      </View>
-      <View style={style.imageContainer}>
-        <Image source={require('../../assets/cheeses/cheese1.png')} style={{resizeMode: 'contain', flex: 1}} />
-      </View>
-      <View>
-        <Text style={{textAlign: 'center', marginTop: 10, backgroundColor: 'red', color: '#fff', fontWeight: '500', fontStyle: 'italic', padding: 10}}>Franche-Comté</Text>
-      </View>
-      <ScrollView style={style.detailsContainer}>
-        <View
-          style={{
-            marginLeft: 20,
-            flexDirection: 'row',
-            alignItems: 'flex-end',
-          }}>
-          <Text style={{fontSize: 16, fontWeight: '400', maxWidth: 300, color: 'orange', fontStyle: 'italic'}}>Comté 12 mois d'affinage</Text>
+    <>
+      <LinearGradient
+        colors={GRADIENT_COLORS}
+        locations={GRADIENT_LOCATIONS}
+        start={START_DEFAULT}
+        end={END_DEFAULT}>
+        <View style={style.header}>
+          <Icon
+            name="arrow-back"
+            style={{fontSize: 32}}
+            onPress={() => navigation.navigate('Home', {screen: 'Cheese'})}
+          />
+          <Icon name="cart" style={{fontSize: 32}} />
         </View>
-        <View
-          style={{
-            marginLeft: 20,
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-            alignItems: 'center',
-          }}>
-          <Text style={{fontSize: 22, fontWeight: 'bold', maxWidth: 270,}}>Comté 12 mois d'affinage</Text>
-          <View style={style.priceTag}>
-            <Text
-              style={{
-                marginLeft: 15,
-                color: COLORS.white,
-                fontWeight: 'bold',
-                fontSize: 16,
-              }}>
-              4.99€
-            </Text>
-          </View>
-        </View>
-        <View style={style.viewAllNotation}>
-          <Text style={style.setTextNotation}>4,9</Text>
+      </LinearGradient>
 
-          <View style={style.viewReviewsCard}>
-            <Text style={style.setStarsReviewsCard}>
-              <Icon
-                name="star"
-                color={'gold'}
-                style={style.setStarsReviewsCard}
-              />
-              <Icon
-                name="star"
-                color={'gold'}
-                style={style.setStarsReviewsCard}
-              />
-              <Icon
-                name="star"
-                color={'gold'}
-                style={style.setStarsReviewsCard}
-              />
-              <Icon
-                name="star"
-                color={'gold'}
-                style={style.setStarsReviewsCard}
-              />
-              <Icon
-                name="star"
-                color={'gold'}
-                style={style.setStarsReviewsCard}
-              />
-            </Text>
-            <Text style={style.setTextReviewsCard}> 479 Avis</Text>
-          </View>
+    <ScrollView
+        style={{
+          flex: 1,
+          backgroundColor: COLORS.white,
+        }}>
+    <View style={style.imageContainer}>
+          <Image
+            source={require('../../assets/cheeses/cheese1.png')}
+            style={{resizeMode: 'contain', flex: 1}}
+          />
         </View>
-        <View style={{paddingHorizontal: 20, marginTop: 15}}>
-          <Text style={{fontSize: 19, fontWeight: 'bold'}}>Informations :</Text>
+        <View>
           <Text
             style={{
-              color: 'grey',
-              fontSize: 16,
-              lineHeight: 22,
-              marginTop: 5,
-              textAlign: 'justify'
+              textAlign: 'center',
+              marginTop: 10,
+              backgroundColor: 'red',
+              color: '#fff',
+              fontWeight: '500',
+              fontStyle: 'italic',
+              padding: 10,
             }}>
-            Potted Plant Ravenea Plant one of the most popular and beautiful species that will produce clumpms. The storage of water often gives succulent plants a more swollen or fleshy appearance than other plants, a characteristic known as succulence. Potted Plant Ravenea Plant one of the most popular.
+            Franche-Comté
           </Text>
-          
-          <View style={{marginTop: 10}}>
-            <Text style={{flex: 1, justifyContent: 'center', alignItems: 'center', fontSize: 18}}>Lait de : <Text style={{color: 'orange', fontWeight: '500', fontSize: 15, fontStyle: 'italic'}}>Vache</Text> </Text>
-            <Text style={{flex: 1, justifyContent: 'center', alignItems: 'center', fontSize: 18}}>Affinage : <Text style={{color: 'orange', fontWeight: '500', fontSize: 15, fontStyle: 'italic'}}>2 mois</Text> </Text>
-            <Text style={{flex: 1, justifyContent: 'center', alignItems: 'center', fontSize: 18}}>Poids : <Text style={{color: 'orange', fontWeight: '500', fontSize: 15, fontStyle: 'italic'}} >500g</Text> </Text>
-            <Text style={{flex: 1, justifyContent: 'center', alignItems: 'center', fontSize: 18}}>Pâte : <Text style={{color: 'orange', fontWeight: '500', fontSize: 15, fontStyle: 'italic'}}>Pâte molle à croûte fleurie</Text> </Text>
-
-          </View>
-
-          <Text style={{fontSize: 19, fontWeight: 'bold', marginTop: 20}}>Quel vin choisir ?</Text>
-          <Text
-            style={{
-              color: 'grey',
-              fontSize: 16,
-              lineHeight: 22,
-              marginTop: 4,
-              textAlign: 'justify'
-            }}>
-            Le vin parfait pour le Comté 12 mois d'affinage est le  <Text style={{color: 'orange', fontWeight: '500',}} >Vin jaune</Text>.
-          </Text>
-
-          <Text style={{fontSize: 19, fontWeight: 'bold', marginTop: 20}}>Recettes liées au fromage :</Text>
-      
-        <ScrollView
-          style={style.setScrollViewNews}
-          horizontal
-          nestedScrollEnabled={true}>
-          <RecipeResume />
-          <RecipeResume />
-          <RecipeResume />
-          <RecipeResume />
-        </ScrollView>
-
-
-
+        </View>
+        <View style={style.detailsContainer}>
           <View
             style={{
-              marginTop: 80,
-              marginBottom: 50,
+              marginLeft: 20,
+              flexDirection: 'row',
+              alignItems: 'flex-end',
+            }}>
+            <Text
+              style={{
+                fontSize: 16,
+                fontWeight: '400',
+                maxWidth: 300,
+                color: 'orange',
+                fontStyle: 'italic',
+              }}>
+              Comté 12 mois d'affinage
+            </Text>
+          </View>
+          <View
+            style={{
+              marginLeft: 20,
               flexDirection: 'row',
               justifyContent: 'space-between',
+              alignItems: 'center',
             }}>
-            <View
-              style={{
-                flexDirection: 'row',
-                alignItems: 'center',
-              }}>
-              <View style={style.borderBtn}>
-                <Text style={style.borderBtnText}>-</Text>
-              </View>
+            <Text style={{fontSize: 22, fontWeight: 'bold', maxWidth: 270}}>
+              Comté 12 mois d'affinage
+            </Text>
+            <View style={style.priceTag}>
               <Text
                 style={{
-                  fontSize: 20,
-                  marginHorizontal: 10,
+                  marginLeft: 15,
+                  color: COLORS.white,
                   fontWeight: 'bold',
+                  fontSize: 16,
                 }}>
-                1
+                4.99€
               </Text>
-              <View style={style.borderBtn}>
-                <Text style={style.borderBtnText}>+</Text>
-              </View>
+            </View>
+          </View>
+          <View style={style.viewAllNotation}>
+            <Text style={style.setTextNotation}>4,9</Text>
+
+            <View style={style.viewReviewsCard}>
+              <Text style={style.setStarsReviewsCard}>
+                <Icon
+                  name="star"
+                  color={'gold'}
+                  style={style.setStarsReviewsCard}
+                />
+                <Icon
+                  name="star"
+                  color={'gold'}
+                  style={style.setStarsReviewsCard}
+                />
+                <Icon
+                  name="star"
+                  color={'gold'}
+                  style={style.setStarsReviewsCard}
+                />
+                <Icon
+                  name="star"
+                  color={'gold'}
+                  style={style.setStarsReviewsCard}
+                />
+                <Icon
+                  name="star"
+                  color={'gold'}
+                  style={style.setStarsReviewsCard}
+                />
+              </Text>
+              <Text style={style.setTextReviewsCard}> 479 Avis</Text>
+            </View>
+          </View>
+          <View style={{paddingHorizontal: 20, marginTop: 15}}>
+            <Text style={{fontSize: 19, fontWeight: 'bold'}}>
+              Informations :
+            </Text>
+            <Text
+              style={{
+                color: 'grey',
+                fontSize: 16,
+                lineHeight: 22,
+                marginTop: 5,
+                textAlign: 'justify',
+              }}>
+              Potted Plant Ravenea Plant one of the most popular and beautiful
+              species that will produce clumpms. The storage of water often
+              gives succulent plants a more swollen or fleshy appearance than
+              other plants, a characteristic known as succulence. Potted Plant
+              Ravenea Plant one of the most popular.
+            </Text>
+
+          <View style={{marginTop: 10}}>
+              <Text
+                style={{
+                  flex: 1,
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  fontSize: 18,
+                }}>
+                Lait de :{' '}
+                <Text
+                  style={{
+                    color: 'orange',
+                    fontWeight: '500',
+                    fontSize: 15,
+                    fontStyle: 'italic',
+                  }}>
+                  Vache
+                </Text>{' '}
+              </Text>
+              <Text
+                style={{
+                  flex: 1,
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  fontSize: 18,
+                }}>
+                Affinage :{' '}
+                <Text
+                  style={{
+                    color: 'orange',
+                    fontWeight: '500',
+                    fontSize: 15,
+                    fontStyle: 'italic',
+                  }}>
+                  2 mois
+                </Text>{' '}
+              </Text>
+              <Text
+                style={{
+                  flex: 1,
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  fontSize: 18,
+                }}>
+                Poids :{' '}
+                <Text
+                  style={{
+                    color: 'orange',
+                    fontWeight: '500',
+                    fontSize: 15,
+                    fontStyle: 'italic',
+                  }}>
+                  500g
+                </Text>{' '}
+              </Text>
+              <Text
+                style={{
+                  flex: 1,
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  fontSize: 18,
+                }}>
+                Pâte :{' '}
+                <Text
+                  style={{
+                    color: 'orange',
+                    fontWeight: '500',
+                    fontSize: 15,
+                    fontStyle: 'italic',
+                  }}>
+                  Pâte molle à croûte fleurie
+                </Text>{' '}
+              </Text>
             </View>
 
-            <View style={style.buyBtn}>
-              <Text
-                style={{color: COLORS.white, fontSize: 18, fontWeight: 'bold'}}>
-                Buy
+            <Text style={{fontSize: 19, fontWeight: 'bold', marginTop: 20}}>
+              Quel vin choisir ?
+            </Text>
+            <Text
+              style={{
+                color: 'grey',
+                fontSize: 16,
+                lineHeight: 22,
+                marginTop: 4,
+                textAlign: 'justify',
+              }}>
+              Le vin parfait pour le Comté 12 mois d'affinage est le{' '}
+              <Text style={{color: 'orange', fontWeight: '500'}}>
+                Vin jaune
               </Text>
-            </View>
+              .
+            </Text>
+
+            <Text style={{fontSize: 19, fontWeight: 'bold', marginTop: 20}}>
+              Recettes liées au fromage :
+            </Text>
+
+        <ScrollView
+              style={style.setScrollViewNews}
+              horizontal
+              nestedScrollEnabled={true}>
+              <RecipeResume />
+              <RecipeResume />
+              <RecipeResume />
+              <RecipeResume />
+            </ScrollView>
+
+            <Text
+              style={{
+                textAlign: 'center',
+                fontSize: 19,
+                fontWeight: 'bold',
+                marginTop: 20,
+              }}>
+              Une question ?
+            </Text>
+            <Text
+              style={{
+                color: 'grey',
+                fontSize: 16,
+                lineHeight: 22,
+                marginTop: 4,
+                textAlign: 'center',
+                paddingBottom: 20,
+              }}>
+              Contactez le support !
+            </Text>
           </View>
         </View>
       </ScrollView>
-    </SafeAreaView>
+    </>
   );
 };
 
 const style = StyleSheet.create({
   header: {
     paddingHorizontal: 20,
-    marginTop: 20,
+    paddingTop: 40,
+    paddingBottom: 10,
     flexDirection: 'row',
     justifyContent: 'space-between',
   },
@@ -220,8 +337,7 @@ const style = StyleSheet.create({
     marginLeft: 32,
   },
   imageContainer: {
-    flex: 0.45,
-    marginTop: 20,
+    flex: 0.8,
     justifyContent: 'center',
     alignItems: 'center',
   },
